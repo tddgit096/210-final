30dfad914b168da0db816a71799b8fe1aa8ecc4e
tddgit096-pc <>
2025-12-10 12:45:26 -0800

    Initial commit.

---
+++ main.cpp
@@ -0,0 +1,28 @@
+// Final Exam I | Toma Dimov | COMSC210
+#include <iostream>
+
+//std:: map
+/*
+Milestone 1: In this assignment's details page, download and install the text file as your data source for this project. In it, each line contains two airport codes representing a particular flight's origin and destination, for example:
+JFK LAX
+ORD DEN
+LAX SFO
+
+Create an std::map<std::string, int> that maps an airport code to its traffic count. Read the file, and for each flight, insert origin and destination airports into the map if they do not exist yet; and increment each airport’s count once.
+Your driver program should build the map from the file and print all airports and their counts in the natural sorted order of the map. 
+
+Milestone 2: Add code that finds the highest traffic count to represent the busiest airport. Print all airports that have this maximum count (in other words, handle ties).
+Your driver program should build the map, print all counts, and then print the busiest airport(s) and their count. 
+
+Milestone 3: Add a function that prints only airports whose traffic counts fall within a specified inclusive range [low, high].
+Your driver program should build the map and call the function at least twice with different ranges, clearly showing which airports fall in each range. Do not code user input. 
+
+Milestone 4: Final polishing, and make sure your driver program exercises all the milestones of this assignment.*/
+
+using namespace std;
+
+int main(){
+
+
+    return 0;
+}
